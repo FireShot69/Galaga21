aba2554c555b63305886cf5d25f4706d132438fb
FireShot69 <nikitapavlatenko50@gmail.com>
2024-05-22 15:20:54 +0300

    Add files via upload

---
+++ управление.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+    char direction;
+
+    int shipPosition = 0;
+
+    while (true) {
+        if (_kbhit()) { //проверяем клавишу
+
+            direction = _getch();
+
+            if (direction == 'a') {
+
+                shipPosition--;
+
+                cout << shipPosition << endl;
+
+            }
+            else if (direction == 'd') {
+
+                shipPosition++;
+
+                cout << shipPosition << endl;
+
+            }
+        }
+    }
+    return 0;
+}
